b431f9ff98e6c2bd28b9378bd7795fb37df88554
kycokpiroga <38364329+kycokpiroga@users.noreply.github.com>
2024-03-03 22:46:35 +0500

    IG worked
    Integrated gradients for a ResNet cat

--- APPLICATION/pages/Integrated gradients.py
+++ APPLICATION/pages/Integrated gradients.py
@@ -1,12 +1,46 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
-import os
 import tensorflow as tf
-from tensorflow.keras.layers import Activation, Conv2D, Dense, Dropout, Flatten, Input, Reshape, MaxPooling2D
-from tensorflow.keras.models import Model
-from tensorflow.keras.utils import to_categorical
-from alibi.explainers import IntegratedGradients
 import matplotlib.pyplot as plt
+from alibi.explainers import IntegratedGradients
+from alibi.datasets import load_cats
+from alibi.utils import visualize_image_attr
 print('TF version: ', tf.__version__)
 print('Eager execution enabled: ', tf.executing_eagerly()) # True
+import tensorflow as tf
+import tensorflow as tf
+from tensorflow.keras.applications.resnet_v2 import ResNet50V2
+
+# Example usage
+model = ResNet50V2(weights='imagenet')
+print("ResNet50V2 model loaded successfully.")
+
+image_shape = (224, 224, 3)
+data, labels = load_cats(target_size=image_shape[:2], return_X_y=True)
+print(f'Images shape: {data.shape}')
+data = (data / 255).astype('float32')
+i = 2
+plt.imshow(data[i])
+model = ResNet50V2(weights='imagenet')
+n_steps = 50
+method = "gausslegendre"
+internal_batch_size = 50
+ig  = IntegratedGradients(model,
+                          n_steps=n_steps, 
+                          method=method,
+                          internal_batch_size=internal_batch_size)
+instance = np.expand_dims(data[i], axis=0)
+predictions = model(instance).numpy().argmax(axis=1)
+explanation = ig.explain(instance, 
+                         baselines=None, 
+                         target=predictions)
+explanation.meta
+explanation.data.keys()
+attrs = explanation.attributions[0]
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
+visualize_image_attr(attr=None, original_image=data[i], method='original_image',
+                    title='Original Image', plt_fig_axis=(fig, ax[0]), use_pyplot=False);
+
+visualize_image_attr(attr=attrs.squeeze(), original_image=data[i], method='blended_heat_map',
+                    sign='all', show_colorbar=True, title='Overlaid Attributions',
+                     plt_fig_axis=(fig, ax[1]), use_pyplot=True);
