9780ccd3e5e01401c0fe49005111d84586abd312
kycokpiroga <38364329+kycokpiroga@users.noreply.github.com>
2024-03-05 19:49:27 +0500

    Create aggrid.py

---
+++ APPLICATION/pages/aggrid.py
@@ -0,0 +1,7 @@
+from st_aggrid import AgGrid
+import pandas as pd
+
+df = pd.read_csv('diabetes.csv')
+#AgGrid(df)
+#https://github.com/TakedaKatsuji/Streamlit-ImageViewer
+#https://github.com/snehankekre/streamlit-d3graph
