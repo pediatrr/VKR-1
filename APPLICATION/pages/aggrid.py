9c7532a1c85016227411dc8a0c9cb8fa3a1dd26b
kycokpiroga <38364329+kycokpiroga@users.noreply.github.com>
2024-03-05 20:41:38 +0500

    Update aggrid.py

--- APPLICATION/pages/aggrid.py
+++ APPLICATION/pages/aggrid.py
@@ -1,7 +1,5 @@
-from st_aggrid import AgGrid
 import pandas as pd
+#from hnet import hnet
+import streamlit as st
 
-df = pd.read_csv('diabetes.csv')
-#AgGrid(df)
-#https://github.com/TakedaKatsuji/Streamlit-ImageViewer
-#https://github.com/snehankekre/streamlit-d3graph
+
