794ba131cb6ed2e81c14c742115406483fa2551e
kycokpiroga <38364329+kycokpiroga@users.noreply.github.com>
2024-03-08 20:17:37 +0500

    shapash_upd
    big uptade foe shapash

--- APPLICATION/pages/shapashM.py
+++ APPLICATION/pages/shapashM.py
@@ -4,12 +4,13 @@ import numpy as np
 from xgboost import XGBRegressor
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 import matplotlib
 from shapash import SmartExplainer
 import io
 import matplotlib.pyplot as plt
-from PIL import Image
 import plotly.express as px
+
 st.set_page_config(page_title="ShapashM", page_icon="🚩")
 st.header('Выберите переменные для модели', divider='rainbow')
 
@@ -22,41 +23,46 @@ X = df.drop(columns='Outcome')
 co = X.columns.tolist()
 yo =  y.values.tolist()
 yo = set(yo)
+multiselect = st.selectbox('Переменная', co)
+
+
+yo =  y.unique().tolist()
+default_values_out = [1]
+multiselect_out = st.multiselect('Класс', yo, default=default_values_out)
+
 #def shapash():
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)  
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)  
 # разбивка
 model = XGBClassifier()
-model.fit(X_train, y_train)
+model.fit(X, y)
 # предсказываем
-y_pred = pd.DataFrame(model.predict(X_test),columns=['Outcome'],index=X_test.index)
-y_test= y_test.astype(int)
-st.write(px.scatter(y_train))
-buf = io.BytesIO()
-plt.savefig(buf, format='png')
-buf.seek(0)
+y_pred = pd.DataFrame(model.predict(X),columns=['Outcome'],index=X.index)
+y= y.astype(int)
 
-image = Image.open(buf)
-st.dataframe(X_train) # для оценки
-st.dataframe(y_pred)
-st.dataframe(y_test)
-
-
+# Filter y_pred based on selected classes
+y_pred_filtered = y_pred[y_pred['Outcome'].isin(multiselect_out)].astype(int)
+X_filtered = X.loc[y_pred_filtered.index]
+max = len(X_filtered)
 xpl = SmartExplainer(model=model)
-xpl.compile(x=X_test,
- y_pred=y_pred,
- y_target=y_test, # Optional: allows to display True Values vs Predicted Values
- )
-st.write(xpl.plot.features_importance())
+xpl.compile(x=X.loc[y_pred_filtered.index],
+            y_pred=y_pred_filtered,
+            y_target=y.loc[y_pred_filtered.index])
 
+st.write(xpl.plot.features_importance())
+st.write(xpl.plot.scatter_plot_prediction())
 
 fig, ax = plt.subplots(figsize=(6, 4))
-st.write(xpl.plot.contribution_plot("BMI"))
 
+st.write(xpl.plot.contribution_plot(multiselect))
+st.write(xpl.plot.top_interactions_plot(nb_top_interactions=5))
 
-st.write(xpl.filter(max_contrib=8,threshold=100))
-st.write(xpl.plot.local_plot(index=23))
+#st.write(xpl.plot.interactions_plot('Sex', 'Pclass'))
+#st.write(xpl.filter(max_contrib=8,threshold=100))
+patient = st.slider('Выберите пациента', min_value=0, max_value=max)
+index = patient
+st.write(xpl.plot.local_plot(index=index))
 summary_df = xpl.to_pandas(
-    max_contrib=3, # Number Max of features to show in summary
+    max_contrib=8, # Number Max of features to show in summary
     threshold=5000,
 )
 st.write(summary_df)
