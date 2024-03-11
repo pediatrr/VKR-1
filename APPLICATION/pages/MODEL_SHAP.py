12dab9d5fdd8f0ce1df7f80aefe384bead006e2b
kycokpiroga <38364329+kycokpiroga@users.noreply.github.com>
2024-03-11 12:04:23 +0500

    Update MODEL_SHAP.py
    many uptd for UO and perfomance

--- APPLICATION/pages/MODEL_SHAP.py
+++ APPLICATION/pages/MODEL_SHAP.py
@@ -19,7 +19,6 @@ multiselect = st.multiselect('Multiselect', co)
 
 def shap():
     from sklearn.model_selection import train_test_split
-    from xgboost import XGBRegressor
     from xgboost import XGBClassifier
     from streamlit_shap import st_shap
     import shap
@@ -29,7 +28,7 @@ def shap():
     X = sm.add_constant(X)
 
     # разбивка
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Делаем модель
     model = XGBClassifier()
@@ -40,37 +39,42 @@ def shap():
     # используем shap для трактования
     explainer = shap.Explainer(model)
     shap_values = explainer(X_test)
-
+    expected_value = explainer.expected_value
+    
     # Суммируем эффекты фич
     #shap.summary_plot(shap_values, X_test)
     X_display=sm.add_constant(df[multiselect])
     # визуализация
     #shap.plots.waterfall(shap_values[0])
-    st_shap(shap.plots.waterfall(shap_values[0]), height=300)
-    st_shap(shap.plots.beeswarm(shap_values), height=300)
+    st_shap(shap.plots.waterfall(shap_values[0]), height=400,width=1250)
+    st_shap(shap.plots.beeswarm(shap_values), height=400,width=1250)
+    st_shap(shap.plots.scatter(shap_values),height=400, width=1250)
+    #st_shap(shap.decision_plot(shap_values))
+
     explainer = shap.TreeExplainer(model)
     shap_values = explainer.shap_values(X)
-    st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], X_display.iloc[0,:]), height=200, width=1000)
-    st_shap(shap.force_plot(explainer.expected_value, shap_values[:1000,:], X_display.iloc[:1000,:]), height=400, width=1000)
+    st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], X_display.iloc[0,:]), height=200, width=1250)
+    st_shap(shap.force_plot(explainer.expected_value, shap_values[:1000,:], X_display.iloc[:1000,:]), height=400, width=1250)
+    #return  shap_values
 if __name__ == "__main__":
     shap()
+    #def ss (shap_values):
+        #st.write(shap.plots.scatter(shap_values[:, shap_values[0,:]], color=shap_values))
+    #ss()
 @st.cache_data
 @st.cache_resource
 def model(multiselect):
     X_selected = sm.add_constant(df[multiselect])
     model= sm.OLS(y, X_selected).fit()
-    #fig = sm.graphics.influence_plot(model, criterion="cooks")
-    #fig.tight_layout(pad=1.0)
-    #st.pyplot(fig)
     fig, ax = plt.subplots(figsize=(4,4))
     fig = sm.graphics.qqplot(model.resid, fit=True, line="45", ax=ax)
     st.pyplot(fig)
-    fig, ax = plt.subplots(figsize=(16,10))
-    fig = sm.graphics.plot_regress_exog(model, 'BMI', fig=fig)
-    st.pyplot(fig)
+    #fig, ax = plt.subplots(figsize=(16,10))
+    #fig = sm.graphics.plot_regress_exog(model, ['Pregnancies','Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age'], fig=fig)
+    #st.pyplot(fig)
 if st.button('Старт',type="primary"):
     model(multiselect)
 # Show and update progress bar
-bar = st.progress(50)
-bar.progress(100)
+#bar = st.progress(50)
+#bar.progress(100)
 
