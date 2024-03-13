87af69c6cf5e9e597cf5f4319a8e315a6eacf971
kycokpiroga <38364329+kycokpiroga@users.noreply.github.com>
2024-03-13 20:34:31 +0500

    LIME uptd
    InterpretMl to LIME

--- APPLICATION/pages/MODEL_LIME.py
+++ APPLICATION/pages/MODEL_LIME.py
@@ -4,11 +4,15 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import streamlit as st
 from statsmodels.formula.api import ols
+import matplotlib.pyplot as plt
+import streamlit
+import streamlit.components.v1 as components
+
 st.set_page_config(page_title="MODEL", page_icon="📊")
 st.markdown("# MODEL Demo LIME")
 st.sidebar.header("MODEl Demo LIME library")
 df = pd.read_csv('diabetes.csv')
-df['Outcome'] = df['Outcome'].astype('category')
+#df['Outcome'] = df['Outcome'].astype('category')
 
 y = df['Outcome']
 X = df.drop(columns='Outcome')
@@ -17,56 +21,33 @@ co = X.columns.tolist()
 st.header('Выберите переменные для модели', divider='rainbow')
 multiselect = st.multiselect('Multiselect', co)
 
+
 def lime ():
     from sklearn.model_selection import train_test_split
-    from xgboost import XGBClassifier
-    import lime
-    import lime.lime_tabular
     X = df[multiselect]
-    y = y = df['Outcome']  
+    y = df['Outcome']  
     # добавим (единица) константа
     X = sm.add_constant(X)
 
     # разбивка
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
+    from interpret import show
+    from interpret import set_visualize_provider
+    from interpret.provider import InlineProvider
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.decomposition import PCA
+    from sklearn.pipeline import Pipeline
+    from interpret.blackbox import LimeTabular
+    pca = PCA()
+    rf = RandomForestClassifier()
 
-    # Делаем модель
-    model = XGBClassifier()
-    model.fit(X_train, y_train)
+    blackbox_model = Pipeline([('pca', pca), ('rf', rf)])
+    blackbox_model.fit(X_train, y_train)
 
-    # предсказываем
-    y_pred = model.predict(X_test)
-
-    selected_patient_data = X.loc[5]
-    explainer = lime.lime_tabular.LimeTabularExplainer(training_data=X_train.values, feature_names=X_train.columns, mode='classification')
-
-    # Получение интерпретации для выбранного пациента
-    explanation = explainer.explain_instance(selected_patient_data.values, model.predict_proba)
-    fig = explanation.as_pyplot_figure()
-    fig.savefig('explanation.png')
-    st.image('explanation.png')
-    # Визуализация важности признаков
-    explanation.show_in_notebook()
+    lime = LimeTabular(blackbox_model, X_train)
+    set_visualize_provider(InlineProvider(detected_envs=['streamlit'])) #Очень Важно
+    streamlit.write(show(lime.explain_local(X_test, y_test)))
+    streamlit.success('Success message')
 if __name__ == "__main__":
     lime()
-# Show and update progress bar
-bar = st.progress(50)
-bar.progress(100)
-'''import streamlit
-set_visualize_provider(InlineProvider(detected_envs=['streamlit'])) #Очень Важно
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.decomposition import PCA
-from sklearn.pipeline import Pipeline
-from interpret.blackbox import LimeTabular
 
-#dl = DecisionListClassifier(feature_names=co)
-
-pca = PCA()
-rf = RandomForestClassifier()
-
-blackbox_model = Pipeline([('pca', pca), ('rf', rf)])
-blackbox_model.fit(X_train, y_train)
-
-lime = LimeTabular(blackbox_model, X_train)
-
-streamlit.write(show(lime.explain_local(X_test, y_test)))'''
