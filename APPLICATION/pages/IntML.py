109832de597b37c0d312324605d47bfefe5f55dc
kycokpiroga <38364329+kycokpiroga@users.noreply.github.com>
2024-04-06 21:09:13 +0500

    Update IntML.py
    big design imp

--- APPLICATION/pages/IntML.py
+++ APPLICATION/pages/IntML.py
@@ -13,7 +13,7 @@ streamlit.set_page_config(page_title="Grad-CAM2+", page_icon="🧊",layout='wide
 from interpret.perf import ROC
 df = pd.read_csv('diabetes.csv')
 #df['Outcome'] = df['Outcome'].astype('category')
-
+col1, col2 = streamlit.columns(2)
 y = df['Outcome']
 X = df.drop(columns='Outcome')
 
@@ -21,40 +21,49 @@ co = X.columns.tolist()
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 ebm = ExplainableBoostingClassifier()
 ebm.fit(X_train, y_train)
+
 ebm_global = ebm.explain_global(name='Глобальный бустер модели')
 set_visualize_provider(InlineProvider(detected_envs=['streamlit'])) #Очень Важно
 #set_visualize_provider(InlineProvider()) неработает
-streamlit.markdown('### Глобальный бустер модели')
-streamlit.write(show(ebm_global))
 
-streamlit.markdown('### Локальный бустер')
+with col1:
+    streamlit.markdown('### Глобальный бустер модели')
+    streamlit.write(show(ebm_global))
+
+    
 ebm_local = ebm.explain_local(X_test, y_test,name='Локальный бустер модели')
-streamlit.write(show(ebm_local)) #локал
-
-ebm_ebm_roc = ROC(ebm.predict_proba).explain_perf(X_test,y_test,name='EBM_ROC')
-streamlit.write(show(ebm_ebm_roc))
-
+with col2:
+    streamlit.markdown('### Локальный бустер')
+    streamlit.write(show(ebm_local)) #локал
 from interpret.glassbox import ClassificationTree
 dt = ClassificationTree(random_state=42)
 dt.fit(X_train, y_train)
 dt_global = dt.explain_global(name='Глобальный бустер модели')
-streamlit.markdown('### Глобальное дерево модели')
-streamlit.write(show(dt_global))
-streamlit.markdown('### Локальное дерево данных')
 
-streamlit.write(show(dt.explain_local(X_test, y_test), 0, name='Локальный бустер модели'))
+with col1:
+    streamlit.markdown('### Глобальное дерево модели')
+    streamlit.write(show(dt_global))
+with col2:    
+    streamlit.markdown('### Локальное дерево данных')
+    streamlit.write(show(dt.explain_local(X_test, y_test), 0, name='Локальный бустер модели'))
 #from sklearn.linear_model import LogisticRegression
 from interpret.glassbox import LogisticRegression
 y=y.astype('category')
 lr = LogisticRegression(max_iter=1000, random_state=42)
 lr.fit(X_train, y_train)
 lr_global = lr.explain_global()
-streamlit.markdown('### Глобальная LOG регрессия')
-streamlit.write(show(lr_global))
-streamlit.markdown('### Локальная LOG регрессия')
-streamlit.write(show(lr.explain_local(X_test, y_test), 0))
+with col1:
+    streamlit.markdown('### Глобальная LOG регрессия')
+    streamlit.write(show(lr_global))
+with col2:
+    streamlit.markdown('### Локальная LOG регрессия')
+    streamlit.write(show(lr.explain_local(X_test, y_test), 0))
 ebm_lr_roc = ROC(lr.predict_proba).explain_perf(X_test,y_test,name='LR_ROC')
-streamlit.write(show(ebm_lr_roc))
+with col1:
+    ebm_ebm_roc = ROC(ebm.predict_proba).explain_perf(X_test,y_test,name='EBM_ROC')
+    streamlit.write(show(ebm_ebm_roc))
+with col2:
+    streamlit.write(show(ebm_lr_roc))
 #streamlit.write(show([lr_global, dt_global])) не работает
 #fi = ebm_global._internal_obj['overall']
 #streamlit.table(fi)
