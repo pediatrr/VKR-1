f524a82c9f07b719a8c39a853cfd66531855ed90
kycokpiroga <38364329+kycokpiroga@users.noreply.github.com>
2024-04-06 20:54:43 +0500

    Update PDPbox.py

--- APPLICATION/pages/PDPbox.py
+++ APPLICATION/pages/PDPbox.py
@@ -17,55 +17,10 @@ X = df.drop(columns='Outcome')
 co = X.columns.tolist()
 yo =  y.values.tolist()
 yo = set(yo)
-multiselect = st.multiselect('Multiselect', co, default=["BMI",'Age'])
-multiselect_out = st.multiselect('Multiselect_output_class', yo, default=1)
+
 #targetfor=y.loc[yo]
 # Делаем модель
-def pdp_1_1():
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)  
-# разбивка
-    model = XGBClassifier()
-    model.fit(X_train, y_train)
-    # предсказываем
-    y_pred = model.predict(X_test)
-    #2.2
-    target_67 = info_plots.InteractTargetPlot(
-        df=df,
-        features=multiselect,
-        feature_names=multiselect,
-        target='Outcome',
-        num_grid_points=10,
-        grid_types='percentile',
-        percentile_ranges=None,
-        grid_ranges=None,
-        cust_grid_points=None,
-        show_outliers=False,
-        endpoints=True,
-    )
 
-    fig, axes, summary_df = target_67.plot(
-        which_classes=multiselect_out,
-        show_percentile=True,
-        figsize=(1200, 400),
-        ncols=2,
-        plot_params={"gaps": {"outer_y": 0.05}},
-        engine='plotly',
-        template='plotly_white',
-    )
-    fig
-
-    fig, axes, summary_df = target_67.plot(
-        which_classes = multiselect_out,
-        show_percentile=True,
-        figsize=(1200, 400),
-        ncols=2,
-        plot_params={"gaps": {"outer_y": 0.05, "top": 0.1}},
-        engine='plotly',
-        template='plotly_white',
-    )
-    fig
-if __name__ == "__main__":
-    pdp_1_1()
 #https://github.com/SauceCat/PDPbox/blob/master/tutorials/pdpbox_binary_classification.ipynb
 
 from sklearn.datasets import load_breast_cancer
@@ -97,5 +52,46 @@ blackbox_model.fit(X_train, y_train)
 pdp = PartialDependence(blackbox_model, X_train)
 set_visualize_provider(InlineProvider(detected_envs=['streamlit'])) #Очень Важно
 
-streamlit.write(show(pdp.explain_global(), 0))
+streamlit.write(show(pdp.explain_global(name='Локальный PDP'), 0))
+from streamlit_shap import st_shap
+import shap
 
+select_p = st.selectbox('Переменная для зависимости', co)
+st_shap(shap.plots.partial_dependence(select_p, blackbox_model.predict,X_test,feature_expected_value=True,ice = False, model_expected_value = True),height=400, width=1250)
+with st.expander('Взаимодействие переменных'):
+    def pdp_1_1():
+        multiselect = st.multiselect('Выбор признаков', co, default=["BMI",'Age'])
+        multiselect_out = st.multiselect('Выбери класс предсказания', yo, default=1)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)  
+    # разбивка
+        model = XGBClassifier()
+        model.fit(X_train, y_train)
+        # предсказываем
+        y_pred = model.predict(X_test)
+        #2.2
+        target_67 = info_plots.InteractTargetPlot(
+            df=df,
+            features=multiselect,
+            feature_names=multiselect,
+            target='Outcome',
+            num_grid_points=10,
+            grid_types='equal',
+            percentile_ranges=None,
+            grid_ranges=None,
+            cust_grid_points=None,
+            show_outliers=False,
+            endpoints=True,
+        )
+
+        fig, axes, summary_df = target_67.plot(
+            which_classes=multiselect_out,
+            show_percentile=True,
+            figsize=(1200, 400),
+            ncols=2,
+            plot_params={"gaps": {"outer_y": 0.05}},
+            engine='plotly',
+            template='plotly_white',
+        )
+        fig
+    if __name__ == "__main__":
+        pdp_1_1()
